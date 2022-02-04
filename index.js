449d438947d355a8f019713cbda7b28abd3f47a1
Akeju Oluwanifemi <akejunifemi11@gmail.com>
2022-02-04 17:34:01 +0100

    1st Commit 2nd Portfolio

---
+++ index.js
@@ -0,0 +1,36 @@
+// function show(){
+//     document.getElementById("bars").style.display= "none";
+//     document.getElementById("times").style.display="block";
+//     document.getElementById("nav").style.display="flex";
+// }
+
+// function hide(){
+//     document.getElementById("bars").style.display="block";
+//     document.getElementById("times").style.display="none";
+//     document.getElementById("nav").style.display="none";
+// }
+
+
+//A new menu bar
+
+function show (){
+    document.getElementById("bars").style.display= "none";
+    document.getElementById("times").style.display="block";
+    document.getElementById("nav").style.width="100vw";
+
+
+
+}
+
+function hide()
+{
+    document.getElementById("bars").style.display= "block";
+    document.getElementById("times").style.display="none";
+    document.getElementById("nav").style.width="0vw";
+    document.getElementById("menuoptions").style.display ="none";
+}
+
+
+
+
+ 
